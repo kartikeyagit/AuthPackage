97afd8badad00d2570e74578ff4bcba39abddccc
Kartikeya Singh <kartikeya85@gmail.com>
2024-07-08 15:55:18 +0530

    Adding Web Service Package

--- Sources/AuthPackage/AuthPackage.swift
+++ Sources/AuthPackage/AuthPackage.swift
@@ -1,2 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public class WebService {
+    
+    
+    public init() {
+        
+    }
+    
+    public func fetchAPI<T: Codable>(url: URL, completion: @escaping (T?) -> Void) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil, (response as? HTTPURLResponse)?.statusCode == 200 else { return completion(nil) }
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completion(result)
+            } catch {
+                completion(nil)
+            }
+            
+        }
+    }
+}
